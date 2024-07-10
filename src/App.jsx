91f9bce8f17668f2105ba5a5b20499e769327c22
rendy <rendyjulfri11@gmail.com>
2024-07-10 11:03:09 +0700

    membuat komponen kotak1 dan kotak2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Kotak1 from "./components/Kotak1"
+import Kotak2 from "./components/Kotak2"
+function App() {
+  return (
+    <>
+    <h1>Hello World!</h1>
+    <Kotak1/>
+    <Kotak2/>
+    </>
+  )
+}
+
+export default App
